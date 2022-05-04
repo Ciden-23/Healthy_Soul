e3d199546c8897a80117595cafc1076c313ecb66
ayeye's servant <sami287borda@gmail.com>
2022-05-03 20:16:20 -0400

    modif principal

--- HealthySoul-1erSprint/Usuario abuelo/menu.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('.nav1');
-window.addEventListener('scroll',function(){
-    nav.classList.toggle('active', window.scrollY >0)
-});
-const nav3 = document.querySelector('.barramenu');
-window.addEventListener('scroll',function(){
-    nav3.classList.toggle('active', window.scrollY >0)
-});
