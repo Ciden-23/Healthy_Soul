4121d224a49ecbdfd790f792f5e29941312e0807
MicaelaTrujillo <102573161+MicaelaTrujillo@users.noreply.github.com>
2022-05-16 17:35:28 -0400

    Archivo inservible

--- Usuario Admin/require.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-//import fs from 'fs';
-
-let archivo = fs.readFileSync('dolencias.txt', 'utf-8');
-console.log(archivo);
-
-
-console.log('Esto se muestra después de haber leído el achivo2.txt (por el readFileSync)');
